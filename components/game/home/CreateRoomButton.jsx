28c6284c8265bee5ac06f51e00e75a4a23441b6d
tzhu65 <tzhu65@gmail.com>
2015-10-23 01:40:18 -0400

    i hope you get this notification

--- components/game/home/CreateRoomButton.jsx
+++ components/game/home/CreateRoomButton.jsx
@@ -5,14 +5,19 @@ var ChangePageActions = require('../../../actions/ChangePageActions');
 var CreateRoomButton = React.createClass({
 
   openNewRoomForm: function() {
-    ChangePageActions.transition('ABOUT');
     console.log('wut wut');
   },
 
   render: function() {
     return (
-      <div id="create-room-button" className="btn btn-default" onClick={this.openNewRoomForm}>
+      <div id="create-room-button"
+           className="btn btn-default"
+           data-toggle="popover"
+           title="Popover title"
+           data-content="And here's some amazing content. It's very engaging. Right?"
+           onClick={this.openNewRoomForm}>
         <span className="glyphicon glyphicon-plus"></span>
+        <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
       </div>
     )
   }
